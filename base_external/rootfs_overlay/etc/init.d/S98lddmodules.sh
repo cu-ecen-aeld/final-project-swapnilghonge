920609615228ff958f407bc4829e9189c570250e
Swapnil Ghonge <36632481+swapnilghonge@users.noreply.github.com>
2022-10-19 16:01:59 -0600

    Update S98lddmodules.sh

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -3,13 +3,16 @@
 case "$1" in
     start)
         modprobe hello.ko
+        modprobe faulty
+        modprobe scull
         
         /usr/bin/module_load faulty
         /usr/bin/scull_load
         ;;
     stop)
         rmmod hello.ko
-
+        rmmod faulty
+        rmmmod scull
         /usr/bin/module_unload faulty
         /usr/bin/scull_unload
         ;;
