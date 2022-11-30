4f347c2c516a8aed265b3b5fda2fccedab5356d3
swapnilghonge <swapnil.ghonge@gmail.com>
2022-11-30 13:19:01 -0700

    Adding Server for buildroot

--- base_external/package/client/client.mk
+++ base_external/package/client/client.mk
@@ -1,20 +1,19 @@
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-SOCKET_VERSION = '2a1660c876009371aadc22a6a849b3a6ab23de5a'
+CLIENT_VERSION = '2a1660c876009371aadc22a6a849b3a6ab23de5a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-SOCKET_SITE = 'https://github.com/swapnilghonge/AESD_swapnil_sricharan_shared'
-SOCKET_SITE_METHOD = git
-SOCKET_GIT_SUBMODULES = YES
+CLIENT_SITE = 'https://github.com/swapnilghonge/AESD_swapnil_sricharan_shared'
+CLIENT_SITE_METHOD = git
+CLIENT_GIT_SUBMODULES = YES
 
-define SOCKET_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/socket_server all
+define CLIENT_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/socket_client all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define SOCKET_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/socket_server/server_socket $(TARGET_DIR)/usr/bin
-	
+	$(INSTALL) -m 0755 $(@D)/socket_client/client $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
