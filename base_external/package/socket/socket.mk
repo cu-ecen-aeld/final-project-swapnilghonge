106487520388be17c4e6d1e31c914e11e9fa65e7
swapnilghonge <swapnil.ghonge@gmail.com>
2022-12-05 21:14:21 -0700

    deleting client and DHT11

--- base_external/package/socket/socket.mk
+++ base_external/package/socket/socket.mk
@@ -1,5 +1,5 @@
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-SOCKET_VERSION = '648ac2c5270090a23da86a31ad73f627596f0cbf'
+SOCKET_VERSION = '809342a2f38f38635f3ec88205ecf1540672406a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -12,6 +12,7 @@ define SOCKET_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/tmp all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/bme all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/socket_client all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/combine all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -21,6 +22,7 @@ define SOCKET_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/tmp/tmp $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/bme/bme $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/socket_client/client $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/combine/combine $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
